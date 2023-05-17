adfda5a54b8eb56bc0d671e43a6cbb140c9f75b7
vlada-tsvek <vladka202@gmail.com>
2023-05-17 19:17:01 +0300

    1home

---
+++ index.js
@@ -0,0 +1,56 @@
+//task 2 (виводить в консоль прізвище)
+// let name="Tsvek";
+// console.log(name);
+
+//task 3
+// let scool , clas;
+// scool="hight ";
+// clas=10;
+// console.log(scool+clas);
+// clas="hight "
+// console.log(scool+clas);
+
+//task 4 (об’єкт з 5-ма властивостями)
+// const user = {
+//     scool: "hight",
+//     clas: 10,
+//     number: true,
+//     num: undefined,
+//     lag: null
+// };
+// console.log(user);
+
+//task 5 (функцію конфірм, Результат записати в змінну isAdult)
+// let isAdult
+// isAdult = confirm("Підтвердіть, що Вам 21 рік")
+// console.log(isAdult);
+
+//task 6 (7 змінних)
+let num, surname, groop, year, isMarried, x, y;
+num=" vlada";
+surname=" tsvek";
+groop=" a";
+year=93;
+isMarried=true;
+console.log(year + " " + isMarried + num + surname + groop);
+x=null;
+y=undefined;
+console.log(x, y);
+
+//task 7 (логін, імейл та пароль)
+// let login , email , password;
+// login = prompt("введіть login");
+// email = prompt("введіть email");
+// password = prompt("введіть пароль пароль");
+// console.log("Dear User, your login " + login + ", your email is " + email + ", your password is " + password);
+
+//task 8 (кількість секунд)
+// let seconnd, hour, day, mounth;
+// second=60;
+// hour=second*60;
+// day=hour*24;
+// mounth=day*30;
+// console.log("second in hour-" + hour);
+// console.log("second in day-" + day);
+// console.log("second in mounth-" + mounth);
+
